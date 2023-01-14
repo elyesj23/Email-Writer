371150c1220187a484e0d0dce998eac882f0cdd3
elyesj23 <106397973+elyesj23@users.noreply.github.com>
2023-01-14 11:35:11 +0100

    deutsch HITLER

--- app.py
+++ app.py
@@ -23,10 +23,10 @@ def index():
         prompt = (
             (f"Type: Email\n"
             f"Von: {flask.request.form['sender']}\n"
-            f"Zu: {flask.request.form['recipient']}\n"
+            f"An: {flask.request.form['recipient']}\n"
             f"Thema: {flask.request.form['subject']}\n"
-            f"Format:Fully written Email\n"
-            f"\nSchreibe eine Email mit 200 Wörtern über  {flask.request.form['message']} in einem {flask.request.form['style']} ton\n"))
+            f"Format:Vollständig geschriebene Email in Deutsch\n"
+            f"\nSchreibe eine Email mit 175 Wörtern über  {flask.request.form['message']}\n"))
 
         
         completions = openai.Completion.create(
@@ -40,24 +40,7 @@ def index():
         print(prompt)
         generated_message = completions.choices[0].text
 
-        url = "https://api-free.deepl.com/v2/translate"
-        headers = {
-            "Authorization": "DeepL-Auth-Key b062df55-dee6-d717-9fb8-fa02cb214fb8:fx",
-            "User-Agent": "YourApp/1.2.3",
-            "Content-Type": "application/x-www-form-urlencoded",
-        }
-
-        data = {
-            "text": generated_message,
-            "target_lang": "de"
-        }
-
-        response = requests.post(url, headers=headers, data=data)
-
-        print(response.status_code)
-        print(response.json())
-        response_json = response.json()
-        translated_text = response_json['translations'][0]['text']
+       
 
 
 
@@ -65,11 +48,11 @@ def index():
 
         # Split the generated message into greeting, beginning, body, and sign-off
         try:
-            greeting, beginning, body, sign_off = translated_text.split("\n\n", 3)
+            greeting, beginning, body, sign_off = generated_message.split("\n\n", 3)
             
         except ValueError:
-            print(translated_text)
-            greeting = translated_text
+            
+            greeting = generated_message
             beginning = ""
             body = ""
             sign_off = ""
@@ -77,7 +60,7 @@ def index():
             
              
 
-        return flask.render_template('index.html', greeting=greeting, beginning=beginning, body=body, sign_off=sign_off, recipient=flask.request.form['recipient'], subject=flask.request.form['subject'], style=flask.request.form['style'], message=flask.request.form['message'])
+        return flask.render_template('index.html', greeting=greeting, beginning=beginning, body=body, sign_off=sign_off, recipient=flask.request.form['recipient'], subject=flask.request.form['subject'], message=flask.request.form['message'])
     return flask.render_template('index.html')
 
 if __name__ == '__main__':
